b5b3655d13b4b9401760684b2654aac29905619b
NudleOS Creator <shawn.jung.f21@gmail.com>
2025-12-27 18:28:20 -0800

    Work on redevelopment

---
+++ nudleos/sys/ct/cts.cpp
@@ -0,0 +1,8 @@
+#define CTS_std
+#include <stdio.h>
+int cts() {
+    void bootcts(int); {
+        printf("The stupidest most boring function ever");
+    }
+    bootcts(5);
+}
---
+++ nudleos/sys/driverinstall.asm
@@ -0,0 +1,10 @@
+add a to b
+_function:
+    push ebp
+    mov ebp, esp
+    sub esp, 
+    leave
+    ret
+%macro  
+    ;This macro is empty for no reason
+%endmacro
